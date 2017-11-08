3b226e07fe3cfa5201343680187a508790b6995b
nvnoahherron <nvnoahherron@gmail.com>
2017-11-07 20:00:18 -0600

    Added buttons for location

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
-	$("button").on("click", function(){
+	var yourCity = [];
+	//on sumbit run function searchLocation
+	$("#search-form").on("submit", searchLocation);
+	//on click run function searchLocation
+	$("#search-button").on("click", searchLocation);
+	//create a function for our event
+	function searchLocation(event){
+		//prevent form from submitting
+		event.preventDefault();
 
-		
 		var API = "e3c7a67e55d75859a8b161362f1a2689";
-		var location = $("#search-input").val();
-		var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial" + "&appid=" + API;
+		var newCity = $("#search-input").val();
+		var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&units=imperial" + "&appid=" + API;
 
 		    $.ajax({
 		      url: queryURL,
@@ -20,14 +27,52 @@ $(document).ready(function() {
 		        $("#weather").append("<h3>Wind Speed: " + response.wind.speed + " MPH</h3>");
 		    });
 
-		    var googleURL = "https://www.googleapis.com/travelpartner/v2.0/068f3741bedde8b1fdf05dc0011708257e539c84/reports";
+		yourCity.push(newCity);
+
+		locationButtons();
+	}
+
+	$(document).on("click", ".newCity", function(){
+
+		$("#weather").empty();
+
+		var theCity = $(this).attr("data-name");
+
+		var API = "e3c7a67e55d75859a8b161362f1a2689";
+
+		var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + theCity + "&units=imperial" + "&appid=" + API;
 
 		    $.ajax({
-		    	url: googleURL,
-		    	method: "GET"
-		    }).done(function(response){
-		    	console.log(response)
+		      url: queryURL,
+		      method: "GET"
+		    }).done(function(response) {
+		    	$("#weather").html("<h1>Current Weather</h1>");
+		        $("#weather").append("<h2>" + response.name + "</h1>");
+		        $("#weather").append("<h3>" + response.weather[0].main + "</h3>");
+		        $("#weather").append("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png'/>");
+		        $("#weather").append("<h3>Humidity: " + response.main.humidity + "%</h3>");
+		        $("#weather").append("<h3>Temperature: " + response.main.temp + "</h3>");
+		        $("#weather").append("<h3>Wind Speed: " + response.wind.speed + " MPH</h3>");
 		    });
 	});
+
+	function locationButtons(){
+
+		$("#recentLocations").empty();
+
+		for (var i = 0; i < yourCity.length; i++) {
+			
+			var city = $("<button>");
+
+			city.addClass("newCity");
+
+			city.attr("data-name", yourCity[i]);
+
+			city.text(yourCity[i]);
+
+			$("#recentLocations").append(city);
+			 console.log(city);
+		}
+	}
 
 })
